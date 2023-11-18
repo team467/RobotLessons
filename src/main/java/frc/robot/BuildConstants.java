389a2e5c4409687e6d7a83422b952299d95d28d7
bryanduerk <bryan.duerk@gmail.com>
2023-11-18 09:53:40 -0500

    Updated AdvantageKit version

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "RobotLessons";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 27;
-  public static final String GIT_SHA = "e5a71e5eff8521171de540acba5057a647612a2c";
-  public static final String GIT_DATE = "2023-10-09 19:23:57 EDT";
+  public static final int GIT_REVISION = 31;
+  public static final String GIT_SHA = "04d4f644afb224bf977eec3a9b2fa3ddc93a1262";
+  public static final String GIT_DATE = "2023-10-09 20:19:58 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2023-10-09 19:57:23 EDT";
-  public static final long BUILD_UNIX_TIME = 1696895843469L;
+  public static final String BUILD_DATE = "2023-11-18 09:52:42 EST";
+  public static final long BUILD_UNIX_TIME = 1700319162031L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
