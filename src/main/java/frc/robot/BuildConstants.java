9278a5804862bdac00dc15859ccb6aeb3ebb23db
Rafia Aleem <rafiaaleem5@gmail.com>
2023-12-13 20:35:23 -0500

    Merged in main branch

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "RobotLessons";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 31;
-  public static final String GIT_SHA = "04d4f644afb224bf977eec3a9b2fa3ddc93a1262";
-  public static final String GIT_DATE = "2023-10-09 20:19:58 EDT";
-  public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2023-12-13 19:37:13 EST";
-  public static final long BUILD_UNIX_TIME = 1702514233163L;
-  public static final int DIRTY = 0;
+  public static final int GIT_REVISION = 32;
+  public static final String GIT_SHA = "a2e1f90cb017e80b4cd4765a8aacfb7a5e28e2a8";
+  public static final String GIT_DATE = "2023-12-13 20:26:09 EST";
+  public static final String GIT_BRANCH = "Rafia";
+  public static final String BUILD_DATE = "2023-12-13 20:34:27 EST";
+  public static final long BUILD_UNIX_TIME = 1702517667809L;
+  public static final int DIRTY = 1;
 
   private BuildConstants(){}
 }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,31 +4,12 @@
 
 package frc.robot;
 
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import frc.lib.utils.AllianceFlipUtil;
-import frc.robot.constants.Constants;
-import frc.robot.subsystems.arm.Arm;
-import frc.robot.subsystems.drive.Drive;
-import frc.robot.subsystems.drive.DriveWithDpad;
-import frc.robot.subsystems.drive.DriveWithJoysticks;
-import frc.robot.subsystems.drive.gyro.GyroIO;
-import frc.robot.subsystems.drive.gyro.GyroIOPigeon2;
-import frc.robot.subsystems.drive.gyro.GyroIOSim;
-import frc.robot.subsystems.drive.wheelpod.WheelPodIO;
-import frc.robot.subsystems.drive.wheelpod.WheelPodIOSim;
-import frc.robot.subsystems.drive.wheelpod.WheelPodIOSparkMax;
 import frc.robot.subsystems.flywheel.FlyWheel;
 import frc.robot.subsystems.flywheel.FlyWheelIO;
 import frc.robot.subsystems.flywheel.FlyWheelIOSim;
 import frc.robot.subsystems.flywheel.FlyWheelIOSparkMax;
-import frc.robot.subsystems.rotator.RotatorIO;
-import frc.robot.subsystems.rotator.RotatorIOPhysical;
-import frc.robot.subsystems.springloadedextender.SpringLoadedExtenderIO;
-import frc.robot.subsystems.springloadedextender.SpringLoadedExtenderIOPhysical;
 import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
 
 /**
@@ -39,11 +20,7 @@ import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
  */
 public class RobotContainer {
 
-  // Subsystems
-  private final Drive drive;
-  private final Arm arm;
-
-  //Flywheel
+  // Flywheel
   private final FlyWheel flywheel;
 
   // Controllers
@@ -62,64 +39,13 @@ public class RobotContainer {
       case REAL -> {
         switch (RobotConstants.get().robot) {
           case ROBOT_COMP -> {
-            // Transform3d front =
-            //     new Transform3d(
-            //         new Translation3d(6 * 0.01, -10 * 0.01 - Units.inchesToMeters(2.0), 42 *
-            // 0.01),
-            //         new Rotation3d());
-            // Transform3d right =
-            //     new Transform3d(
-            //         new Translation3d(2 * 0.01, -12 * 0.01 - Units.inchesToMeters(2.0), 42 *
-            // 0.01),
-            //         new Rotation3d(0, 0, -0.5 * Math.PI));
-            drive =
-                new Drive(
-                    new GyroIOPigeon2(17),
-                    new WheelPodIOSparkMax(3, 4, 13, 0),
-                    new WheelPodIOSparkMax(5, 6, 14, 1),
-                    new WheelPodIOSparkMax(1, 2, 15, 2),
-                    new WheelPodIOSparkMax(7, 8, 16, 3));
-            // List.of(
-            //     new VisionIOAprilTag("front", front, FieldConstants.aprilTags),
-            //     new VisionIOAprilTag("right", right, FieldConstants.aprilTags)));
-            // arm =
-            //     new Arm(
-            //         new SpringLoadedExtenderIOPhysical(
-            //             RobotConstants.get().armExtendMotorId,
-            //             RobotConstants.get().ratchetSolenoidId),
-            //         new RotatorIOPhysical(RobotConstants.get().armRotateMotorId));
             flywheel = new FlyWheel(new FlyWheelIO() {});
-            // effector =
-            //     new Effector(
-            //         new EffectorIOBrushed(
-            //             RotConstants.get().intakeMotorID(),
-            //             RobotConstants.get().intakeCubeLimitSwitchID()));
           }
           case ROBOT_BRIEFCASE -> {
-            // drive =
-            //     new Drive(
-            //         new GyroIO() {},
-            //         new WheelPodIO() {},
-            //         new WheelPodIO() {},
-            //         new WheelPodIO() {},
-            //         new WheelPodIO() {});
-            // List.of(new VisionIO() {}));
-            //arm = new Arm(new SpringLoadedExtenderIO() {}, new RotatorIO() {});
-            // effector = new Effector(new EffectorIO() {});
             flywheel = new FlyWheel(new FlyWheelIOSparkMax(1, 25));
           }
           default -> {
-            drive =
-                new Drive(
-                    new GyroIO() {},
-                    new WheelPodIO() {},
-                    new WheelPodIO() {},
-                    new WheelPodIO() {},
-                    new WheelPodIO() {});
-            // List.of(new VisionIO() {}));
-            arm = new Arm(new SpringLoadedExtenderIO() {}, new RotatorIO() {});
-                        flywheel = new FlyWheel(new FlyWheelIO() {});
-            // effector = new Effector(new EffectorIO() {});
+            flywheel = new FlyWheel(new FlyWheelIO() {});
           }
         }
       }
@@ -127,33 +53,15 @@ public class RobotContainer {
       case SIM -> {
         // Init subsystems
         // subsystem = new Subsystem(new SubsystemIOSim());
-        drive =
-            new Drive(
-                new GyroIOSim() {},
-                new WheelPodIOSim(),
-                new WheelPodIOSim(),
-                new WheelPodIOSim(),
-                new WheelPodIOSim());
-        // List.of(new VisionIO() {}));
-        arm = new Arm(new SpringLoadedExtenderIO() {}, new RotatorIO() {});
+
         flywheel = new FlyWheel(new FlyWheelIOSim());
-        // effector = new Effector(new EffectorIO() {});
       }
 
         // Replayed robot, disable IO implementations
       default -> {
         // subsystem = new Subsystem(new SubsystemIO() {});
-        drive =
-            new Drive(
-                new GyroIOSim() {},
-                new WheelPodIOSim(),
-                new WheelPodIOSim(),
-                new WheelPodIOSim(),
-                new WheelPodIOSim());
-        // List.of(new VisionIO() {}));
-        arm = new Arm(new SpringLoadedExtenderIO() {}, new RotatorIO() {});
-                    flywheel = new FlyWheel(new FlyWheelIO() {});
-        // effector = new Effector(new EffectorIO() {});
+
+        flywheel = new FlyWheel(new FlyWheelIO() {});
       }
     }
     configureButtonBindings();
@@ -166,74 +74,10 @@ public class RobotContainer {
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
   private void configureButtonBindings() {
-
-    // driverController.y().onTrue(Commands.runOnce(() -> isRobotOriented = !isRobotOriented));
-
-    // drive.setDefaultCommand(
-    //     new DriveWithJoysticks(
-    //         drive,
-    //         () -> -driverController.getLeftY(),
-    //         () -> -driverController.getLeftX(),
-    //         () -> -driverController.getRightX(),
-    //         () -> isRobotOriented // TODO: add toggle
-    //         ));
-    // driverController
-    //     .start()
-    //     .onTrue(
-    //         Commands.runOnce(
-    //                 () ->
-    //                     drive.setPose(
-    //                         new Pose2d(
-    //                             drive.getPose().getTranslation(),
-    //                             AllianceFlipUtil.apply(new Rotation2d()))))
-    //             .ignoringDisable(true));
-    // driverController
-    //     .pov(-1)
-    //     .whileFalse(new DriveWithDpad(drive, () -> driverController.getHID().getPOV()));
-
-    // led2023.setDefaultCommand(new LedRainbowCMD(led2023).ignoringDisable(true));
-    // effector.setDefaultCommand(new HoldCMD(effector));
-
-    // driverController.leftBumper().toggleOnTrue(new IntakeAndRaise(arm, effector));
-    // driverController.rightBumper().toggleOnTrue(new ReleaseCMD(effector, arm));
-
-    // Set the game piece type
-    // operatorController.back().whileFalse(new WantConeCMD(effector));
-    // operatorController.back().whileTrue(new WantCubeCMD(effector));
-
-    // Manual arm movements
-    // operatorController.pov(90).whileTrue(arm.manualExtend());
-    // operatorController.pov(270).whileTrue(arm.manualRetract());
-    // operatorController.pov(0).whileTrue(arm.manualUp());
-    // operatorController.pov(180).whileTrue(arm.manualDown());
-
-    // // Placing cone or cube, gets what it wants from in the command
-    // operatorController.a().onTrue(new ArmScoreLowNodeCMD(arm));
-    // operatorController.b().onTrue(new ArmScoreMidNodeCMD(arm, effector::wantsCone));
-    // operatorController.y().onTrue(new ArmScoreHighNodeCMD(arm, effector::wantsCone));
-
-    // // Home will be for movement
-    // operatorController.x().onTrue(new ArmHomeCMD(arm, effector::wantsCone));
-    // driverController.x().onTrue(new ArmHomeCMD(arm, effector::wantsCone));
-
-    // Need to set to use automated movements, should be set in Autonomous init.
-    // driverController.back().onTrue(arm.calibrate());
-    // driverController.b().onTrue(arm.forceCalibrated());
-
-    // driverController.a().onTrue(Commands.runOnce(() -> drive.stopWithX(), drive));
-
-    // // Manual arm movements
-    // operatorController.leftStick().onTrue(new ArmStopCMD(arm));
-    // operatorController.rightStick().onTrue(new ArmStopCMD(arm));
-    // operatorController.leftBumper().onTrue(new ArmShelfCMD(arm, effector));
-    // operatorController.rightBumper().onTrue(new ArmFloorCMD(arm, effector));
-
-    // // Auto grid align
-    // driverController.rightTrigger().whileTrue(new NewAlignToNode(drive, effector));
-
     // Flywheel
     operatorController.a().whileTrue(flywheel.startCommand());
   }
+
   /** Runs post-creation actions and eliminates warning for not using the RobotContainer. */
   public void init() {}
 
