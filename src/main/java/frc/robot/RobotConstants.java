188cb52f8f962dc20132a66572b380b71be0c0d3
Bryan Duerk <bryan.duerk@gmail.com>
2023-12-18 11:23:11 -0500

    Constants should set automatically

--- src/main/java/frc/robot/RobotConstants.java
+++ src/main/java/frc/robot/RobotConstants.java
@@ -10,7 +10,8 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class RobotConstants {
-  private static Constants constants = new BriefcaseConstants();
+  
+  private static Constants constants = null;
 
   private RobotConstants() {
     throw new IllegalStateException("Utility class");
